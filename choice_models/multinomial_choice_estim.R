de0539b59598770ef640953acdd825d6ae7bbc49
Frederik Bennhoff <frederik@bennhoff.de>
2022-10-08 18:59:16 +0200

    add a function to calculate mixed mulitnomial logit choice probabilities

--- choice_models/multinomial_choice_estim.R
+++ choice_models/multinomial_choice_estim.R
@@ -16,6 +16,7 @@ require(utils)
 require(EnvStats)
 require(rlang)
 require(tictoc)
+require(mgcv)
 
 # laod data
 choices <- read.csv("./data/choices.csv")
@@ -27,8 +28,8 @@ params <- list(
   n_attr = 2
 )
 
-
-# Multinomial Probit: homogeneous coefficients
+### ------------------------------------------------------------------------ ###
+# Multinomial Probit: homogeneous coefficients ####
 # Simulate choice probabilities
 p_mprob <- function(j, coefs, attributes, cov0, nsim = 10000) {
   #
@@ -80,28 +81,85 @@ p_mprob <- function(j, coefs, attributes, cov0, nsim = 10000) {
 
 }
 
+### ------------------------------------------------------------------------ ###
+# Mixed Multinomial Logit Model: Heterogeneous coefficients ####
+# Simulate choice Probabilities
+p_mmlogit_norm <- function(j, cmean, ccov, attributes, nsim = 10000){
+  # 
+  # Description: Simulate the probability of objects j being chosen if coefficients
+  #              on attributes (i.e. choice paramters) are normally distributed 
+  #              in the population, with mean cmean and covariance ccov.
+  #              Let na be the number of attributes of each alternative
+  #              and nc be the number of alternatives to be chosen from.
+  #
+  # nsim (int):  number of samples drawn for simulation
+  # ccov (mat):  [na x na] matrix
+  # alternatives (mat): [nc x na] matrix
+  # j (int/vec): calculate choice probabilities for these objects. 
+  #
+  
+  ζ <- rmvn(nsim, cmean, ccov) # sampled coefficients
+  sampled_probs <- calc_choice_prob_given_parameters_mmlogit(j, ζ, attributes) 
+  if (is.vector(sampled_probs)) sampled_probs <- t(as.matrix(sampled_probs))
+  return(rowMeans(sampled_probs))
+}
+calc_choice_prob_given_parameters_mmlogit <- function(j, coefs, attributes){
+  # function is vectorized using df operations
+  # j can be a single alternative or a vector of alternatives.
+  # Two cases: coefficient vector is provided, or a matrix of coefficients with
+  # each row corresponding to one sampled coefficient vector is provided.
+  if (is.vector(coefs)) {
+    vΩ <- exp(attributes %*% coefs)
+    SvΩ <- exp(vΩ) %>% sum
+    return(exp(vΩ)[j] / SvΩ )
+  }
+  else if (is.matrix(coefs)) {
+    vΩ <- exp(attributes %*% t(coefs))
+    SvΩ <- exp(vΩ) %>% colSums
+    return(exp(vΩ[j,]) / SvΩ)
+  }
+  else {
+    stop("Cannot work with input coefficients.")
+  }
+}
+
+
+### ------------------------------ Sandbox --------------------------------- ###
 
 ## Not run:
-
 # set some parameters and run the function for a choice of j ∈ {1, 2, 3}
-attributes  <- matrix(c(-.2, 1 , .3, .2, -.1, -.6), ncol = 2)
+# parameters for mprobit and mixed mlogit.
+attributes  <- matrix(c(-.2, .12 , .3, .2, -.1, -.6), ncol = 2)
 coefs       <- c( 2  , 0.1)
-cov0 <- matrix(c(  1,  .5,  .5,  
+cov0        <- matrix(c(  1,  .5,  .5,  
                   .5, 1  , 0.2, 
                   .5, 0.2,   1 ), ncol = 3)
+# parameters for mixed logit only.
+cmean       <- c(0.2,0)
+ccov        <- matrix(c(1, .5, .5, 1), ncol = 2)
 
+# 1. Try out the probability simulator for the multinomial mixed logit model.
+cat("\n---- 1 ----", sep = "\n")
+tic("simulate all choice probabilities in the mixed multinomial logit model.")
+p_mmlogit_norm(1:nrow(attributes), cmean, ccov, attributes, nsim = 10000) %>% cat(sep = "\n")
+toc()
+
+# 2. Try out the probability simulator for the multinomial probit model.
 j <- 2 # index of chosen alternative
-tic("simulate a choice probability")
-p_mprob(3, coefs, attributes, cov0, nsim = 1000000)
+cat("\n---- 2a ----", sep = "\n")
+tic("simulate a choice probability in the multinomial probit model.")
+p_mprob(3, coefs, attributes, cov0, nsim = 10000) %>% cat(sep = "\n")
 toc()
 
 # set parameters as to simulate a fully arbitrary choice 
 # and run the function for a choice of j ∈ {1, ..., 5}
 # choice probability should be around .2
+cat("\n---- 2b ----", sep = "\n")
+tic("simulate a choice probability in the multinomial probit model, \nequal likelihood case.")
 p_mprob(1, coefs = c(1), 
         attributes = matrix(rep(0, 5), ncol = 1), 
         cov0 = diag(5), 
-        nsim = 1000000)
-
+        nsim = 10000) %>% cat(sep = "\n")
+toc()
 
 ## End(Not run)
